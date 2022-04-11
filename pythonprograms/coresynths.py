8cf7ace92219e2b58b266cfbd28cfeebdd0f481a
Ralt144MI <75539795+Rhoumi@users.noreply.github.com>
2022-04-11 16:51:24 +0200

    Update coresynths.py

--- pythonprograms/coresynths.py
+++ pythonprograms/coresynths.py
@@ -8,34 +8,6 @@ import subprocess
 import platform
 import shutil
 
-
-
-#JSON#####################
-
-def read_json(json_file):
-        f = open(json_file)
-        data = json.load(f)
-        f.close()
-        return data
-  #  else:
-  #      sys.exit("Could not find json file %s" % json_file)
- #       
-# Some parts of the generated json file are parsed as lists of dicts -
-# This flattens one of those into one dictionary
-def flatten_list_of_dicts(list_of_dicts):
-    return ChainMap(*list_of_dicts)
-
-def write_file(file, contents):
-    f = open(file, "w")
-    f.write(contents)
-    f.close()
-
-def make_dir(dir_path):
-    if not path.exists(dir_path):
-        os.mkdir(dir_path)
-        
-#####################
-
 def get_coresynths_filepath(filename:str ='core-synths.scd'):
     """ 
     Return the filepath of core-synths.scd. 
@@ -46,20 +18,34 @@ def get_coresynths_filepath(filename:str ='core-synths.scd'):
 
  
 
-def placeholder_filler(synth_name: str, argument_list: list):
+def placeholder_filler(synth_name: str, nb_inputs : int, argument_list: list):
     """ Inserting arguments in placeholder SynthDef """
     template = '''\n    SynthDef(\"{synth_name}\" ++ ~dirt.numChannels, {{
-        | {argument_list}|
+        | {in_list},{argument_list}|
         var signal = In.ar(out, ~dirt.numChannels);
-        signal = {synth_name}.ar({signal_argument}, out);
+        signal = {synth_name}.ar({signal_beg},{signal_argument}, out);
         ReplaceOut.ar(out, signal);
          }}).add;
     \n
     '''
+    ###add one "signal" by inputs
+    signal_beg="signal"
+    for x in range(1, nb_inputs, 1):
+        signal_beg = signal_beg+",signal"
+        
+    ###add in%s by inputs
+    in_list = "in0"
+    for x in range(1, nb_inputs, 1):
+        in_list = in_list+",in%s" % x
+    
+    
     argument_beautify = ', '.join(argument_list)
     return template.format(synth_name=synth_name, 
                            argument_list=argument_beautify,
-                           signal_argument=argument_beautify)
+                           signal_argument=argument_beautify,
+                           nb_inputs=nb_inputs,
+                           signal_beg=signal_beg,
+                           in_list=in_list)
 
 def find_last_occurence(filepath: str, pattern: str):
     """ Find the last occurence of pattern in file and return line """
@@ -77,9 +63,9 @@ def inject_new_definition(text_content: str, filepath: str):
     """ Inject new definition in core-synths.scd """
     index = find_last_occurence(filepath, pattern=".add;")
 
-    # Looking for the last .add in file, adding right after it
+    # Looking for the last .add in file, adding right after it
 
-    # Reading file into memory
+    # Reading file into memory
     with open(filepath, "r") as f:
         contents = f.readlines()
 
@@ -93,93 +79,10 @@ def inject_new_definition(text_content: str, filepath: str):
 
     return True
 
-def get_parameter_list(json_data, with_initialization):
-    out_string = ""
-    # The zero index is needed because it's all in the first index, or is it? @FIXME
-    counter=0
-
-    inputs = ""
-    if json_data["inputs"] > 0:
-        for i in range(json_data["inputs"]):
-            if i != 0:
-                inputs = inputs + ", in%s" % i
-            else:
-                inputs = inputs + "in%s" % i
-
-    for ui_element in json_data["ui"][0]["items"]:
-
-        param_name=""
-        if "label" in ui_element:
-            param_name = sanitize_label(ui_element["label"])
-
-        param_default = ""
-        if "init" in ui_element:
-            param_default = ui_element["init"]
-        else:
-            param_default = "0"
-
-        # Param name
-        if with_initialization:
-            this_argument =  "%s(%s)" % (param_name, param_default)
-        else:
-            this_argument = param_name
-
-        this_argument = this_argument.replace(" ", "_")
-        if counter != 0:
-            out_string = out_string + ", " + this_argument
-        else:
-            out_string = this_argument
-
-        counter = counter + 1
-
-    if json_data["inputs"] > 0:
-        if out_string == "":
-            out_string = inputs
-        else:
-            out_string = inputs + "," + out_string
-
-    return out_string
-
-# This sanitizes the "name" field from the faust file, makes it capitalized, removes dashes and spaces
-def get_class_name(json_data, noprefix):
-    # Capitalize all words in string
-
-    name = dsp_name(json_data)
-
-    if noprefix != 1:
-        name = "Faust" + name
-
-    # Max length of name is 31
-    if len(name) > 31:
-        name = name[0:30]
-
-    return name
-
-
-
-def normalizeClassName(meta_name):
-    upnext=True
-    normalized=""
-    for char in meta_name:
-        if upnext:
-            normalized = normalized + char.upper()
-            upnext=False
-            continue
-        if char == "_" or char=="-" or char==" ":
-            upnext=True
-        else:
-            normalized = normalized + char
-
-    maxlen = 31
-    return normalized[0:maxlen-1]
-
-def dsp_name(json_data):
-    name  = normalizeClassName(json_data["name"])
-    return name
-
 if __name__ == "__main__":
     FILEPATH = get_coresynths_filepath()
     find_last_occurence(filepath=FILEPATH, pattern="add;")
-    new_def = placeholder_filler(synth_name =  "lol",
-            argument_list = ["in", "in2", "out"])
+    new_def = placeholder_filler(synth_name =  "FAUSTDSP",
+              nb_inputs = 2,                  
+              argument_list = ["param1","param2","param3","param4","param5"])
     inject_new_definition(text_content=new_def, filepath=FILEPATH)
