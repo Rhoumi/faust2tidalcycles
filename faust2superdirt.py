255deb9204d4a74f3c94650c73c355c41ffb1c59
Ralt144MI <75539795+Rhoumi@users.noreply.github.com>
2022-04-14 10:43:20 +0200

    Update faust2superdirt.py

--- faust2superdirt.py
+++ faust2superdirt.py
@@ -8,18 +8,19 @@ import subprocess
 import platform
 import shutil
 
-#####################################
-######▄▄▄#▄▄▄▄▄▄▄#▄▄▄▄▄▄▄#▄▄####▄#### 
-#####█   █       █       █  █##█ █###
-#####█   █  ▄▄▄▄▄█   ▄   █   █▄█ █###
-##▄##█   █ █▄▄▄▄▄█  █#█  █       █###
-#█ █▄█   █▄▄▄▄▄  █  █▄█  █  ▄    █###
-#█       █▄▄▄▄▄█ █       █ █#█   █###
-#█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█##█▄▄█###
-#####################################
+
+######################################
+#######▄▄▄#▄▄▄▄▄▄▄#▄▄▄▄▄▄▄#▄▄####▄#### 
+######█   █       █       █  █##█ █###
+######█   █  ▄▄▄▄▄█   ▄   █   █▄█ █###
+###▄##█   █ █▄▄▄▄▄█  █#█  █       █###
+##█ █▄█   █▄▄▄▄▄  █  █▄█  █  ▄    █###
+##█       █▄▄▄▄▄█ █       █ █#█   █###
+##█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█##█▄▄█###
+######################################
 
 # Opening JSON file
-with open('testvgroup.dsp.json') as json_file:
+with open('/home/ralt144mi/Documents/GRAME/TESTPY/testeffet.dsp.json') as json_file:
     json_data = json.load(json_file)
 #with open('testvgroup.dsp.json','r') as string:
  #   json_data_string = json.load(string)
@@ -32,54 +33,58 @@ with open('testvgroup.dsp.json') as json_file:
  
     # Print the data of dictionary
     
-    my_inputs = 1#json_data['inputs']
-    my_outputs = 1#json_data['outputs']
-    my_name = 'yo'#json_data['name']
+    my_inputs = json_data['inputs']
+    my_outputs = json_data['outputs']
+    my_name = json_data['name']
 
     
     
     addresses = ''
     
-    
-def incr(y: int):
-    """ Inserting formated jsondata """
-    template = '''json_data["ui"][0]["items"]{basebase}[0]["type"]
-    '''
-    basebase=""
-    for x in range(0, y, 1) :
-        basebase = basebase+'''[x]["items"]'''
 
-    return template.format(y=y,
-                          basebase=basebase)
-     
-    
-def searching_keyskeys(x, base) :
-    for x in range(0,32) :
-        if base == 'vslider' or base == 'hslider' :
-            print("trouvé" + base)
-        elif base == 'vgroup' or base == 'hgroup' :
-            searching_keyskeys(x, incr(x))
-            print(x)
-            print(base)
-        else :
-            print("nul")
-            print("nope")
-    
-def searching_keys(x) :
-        if json_data["ui"][0]["items"][0]["type"] == 'vslider' or json_data["ui"][0]["items"][0]["type"] == 'hslider' :
-            print("trouvé" + json_data["ui"][0]["items"][0]["label"])
-        else :
-            print("+1")
-            searching_keyskeys(x, incr(x))
-    
-#for x in range(0, 32, 1):
-#        searching_keys(x)              
-        
-searching_keyskeys(0, incr(0))  
-print(json_data["ui"][0]["items"][0]["type"])
+PARAMETER = ["hslider", "vslider", "hbargraph", "vbarbraph",
+                            "nentry", "checkbox", "button"]
+GROUP = ["vgroup", "hgroup", "tgroup"]
 
-  
+def flatten(container):
+    """ Helper function to flatten arbitrarily nested lists """
+    for i in container:
+        if isinstance(i, (list,tuple)):
+            for j in flatten(i):
+                yield j
+        else:
+            yield i
 
+def json_to_ui_data(json_data: dict):
+    """ Return the UI part of the Faust-generated JSON """
+    return json_data["ui"][0]
+
+def item_list_processor(items_value: list):
+    """ Process data contained in "items" keys """
+    parameter_list = []
+    # On parcourt chaque dictionnaire que l'on trouve dans la liste "items"
+    for items in items_value:
+        if isinstance(items, dict):
+            # Si on voit que le type correspond à un param, on gagne un param
+            if items["type"] in PARAMETER:
+                parameter_list.append(items["label"])
+            # Sinon, cela signifie que l'on doit 
+            # continuer à descendre pour extraire
+            elif items["type"] in GROUP:
+                parameter_list.append(item_list_processor(items["items"]))
+        else:
+            pass
+    return parameter_list
+
+def parameter_gatherer(data):
+    """ JSON Parameter extractor """
+    parameter_list = []
+    for key, value in data.items():
+        if key == "items":
+            parameter_list.append(item_list_processor(value))
+        else:
+            continue
+    return parameter_list
 
     
  #WIP   
@@ -94,6 +99,65 @@ print(json_data["ui"][0]["items"][0]["type"])
     print(my_arguments)
     
  """
+def get_header_paths(headerpath):
+    folders = [
+        path.join(headerpath, "plugin_interface"),
+        path.join(headerpath, "server"),
+        path.join(headerpath, "common")
+    ]
+
+    if all(path.exists(folder) for folder in folders):
+        print("Found SuperCollider headers: %s" % headerpath)
+        return folders
+
+# Try and find SuperCollider headers on system
+def find_headers(headerpath):
+    folders = get_header_paths(headerpath)
+    if folders:
+        return folders
+
+    # Possible locations of SuperCollider headers
+    guess = [
+        "/usr/local/include/SuperCollider",
+        "/usr/local/include/supercollider",
+        "/usr/include/SuperCollider",
+        "/usr/include/supercollider",
+        "/usr/local/include/SuperCollider/",
+        "/usr/share/supercollider-headers",
+        path.join(os.getcwd(), "supercollider")
+    ]
+
+    if 'HOME' in os.environ:
+        guess.append(path.join(os.environ['HOME'], "supercollider"))
+
+    for headerpath in guess:
+        folders = get_header_paths(headerpath)
+        if folders:
+            return folders
+
+    sys.exit("Could not find SuperCollider headers")
+
+    
+find_headers("./include")
+
+def find_file(file_name, directory_name):
+    files_found = []
+    for path, subdirs, files in os.walk(directory_name):
+        for name in files:
+            if(file_name == name):
+                file_path = os.path.join(path,name)
+                files_found.append(file_path)
+    return files_found
+
+
+cs_loc = ''.join(find_file('core-synths.scd', '/home/ralt144mi/.local/share/SuperCollider'))
+cm_loc = ''.join(find_file('core-modules.scd', '/home/ralt144mi/.local/share/SuperCollider'))
+bt_loc = ''.join(find_file('BootTidal.hs', '/home/ralt144mi/.cabal/share'))
+
+print(cs_loc)
+print(cm_loc)
+print(bt_loc)
+
     
 """ 
  / \------------------, 
@@ -103,6 +167,7 @@ print(json_data["ui"][0]["items"][0]["type"])
     \_/_______________/     
     
 """
+
 def get_coresynths_filepath(filename:str ='core-synths.scd'):
     """ 
     Return the filepath of core-synths.scd. 
@@ -116,7 +181,7 @@ def get_coresynths_filepath(filename:str ='core-synths.scd'):
 def cs_placeholder_filler(synth_name: str, nb_inputs : int, argument_list: list):
     """ Inserting arguments in placeholder SynthDef """
     template = '''\n    SynthDef(\"{synth_name}\" ++ ~dirt.numChannels, {{
-        | {in_list},{argument_list}|
+        | out, {in_list},{argument_list}|
         var signal = In.ar(out, ~dirt.numChannels);
         signal = {synth_name}.ar({signal_beg},{signal_argument}, out);
         ReplaceOut.ar(out, signal);
@@ -273,7 +338,7 @@ def bt_placeholder_filler(synth_name: str, argument_list: list):
     #and if Param1 in json is float make 
     # elif type(x) == float :
                                 #but dont know yet how to acces json, but soon i hope
-    parameters = ["{x} = pF {x}".format(x=x) for x in argument_list]
+    parameters = ['{x} = pF ""{x}""'.format(x=x) for x in argument_list]
     # weird formatting to match default file
     parameters = "\n     ".join(parameters)
     parameters = parameters.replace('"', '')
@@ -316,27 +381,38 @@ def bt_inject_new_definition(text_content: str, filepath: str):
 
 if __name__ == "__main__":
     
+#json
+
+    json_data = json_to_ui_data(json_data)
+    param = parameter_gatherer(json_data)
+    param = list(flatten(param))
+    print(param)
+    
         
 #coresynth
     
-    cs_FILEPATH = get_coresynths_filepath()
+   # cs_FILEPATH = get_coresynths_filepath()
+    cs_FILEPATH = cs_loc
     cs_find_last_occurence(filepath=cs_FILEPATH, pattern="add;")
     new_def = cs_placeholder_filler(synth_name =  my_name,
               nb_inputs = my_inputs,                  
-              argument_list = ["param1","param2","param3","param4","param5"])
+              argument_list = param)
     cs_inject_new_definition(text_content=new_def, filepath=cs_FILEPATH)
 
 #coremodules
 
-    cm_FILEPATH = get_coremodules_filepath()
+    #cm_FILEPATH = get_coremodules_filepath()
+    cm_FILEPATH = cm_loc
     cm_find_penultimate_occurence(filepath=cm_FILEPATH, pattern=");")
     new_def = cm_placeholder_filler(synth_name = my_name,
-              argument_list = ["param1","param2","param3","param4","param5"])
+              argument_list = param)
     cm_inject_new_definition(text_content=new_def, filepath=cm_FILEPATH)
 
 #boottidal 
-    bt_FILEPATH = get_boottidal_filepath()
+
+    #bt_FILEPATH = get_boottidal_filepath()
+    bt_FILEPATH = bt_loc
     bt_find_penultimate_occurence(filepath=bt_FILEPATH, pattern="add;")
     new_def = bt_placeholder_filler(synth_name = my_name,
-              argument_list = ["param1","param2","param3","param4","param5"])
+              argument_list = param)
     bt_inject_new_definition(text_content=new_def, filepath=bt_FILEPATH)
